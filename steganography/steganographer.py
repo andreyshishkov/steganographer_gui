d6762966e4de54c39c9e271fa14db76b18cd537d
Andrey Shishkov <shishkoffandrey@yandex.ru>
2024-12-09 04:10:30 +0300

    Add func deserialize_data

--- steganography/steganographer.py
+++ steganography/steganographer.py
@@ -20,3 +20,12 @@ def serialize_data(data: bytes, padding: int = 1):
         serialized_data.append(0)
 
     return serialized_data
+
+
+def deserialize_data(data: list) -> bytes:
+    """Takes data and unpacks the 2-bits groups into original'"""
+    deserialized_data = list()
+    for i in range(len(data) - 4 + 1, 4):
+        datum = (data[i] << 6) + (data[i + 1] << 4) + (data[i + 2] << 2) + (data[i + 3] << 0)
+        deserialized_data.append(datum)
+    return bytes(deserialized_data)
